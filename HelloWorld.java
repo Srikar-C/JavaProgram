8d3abb4753b7d4827203c66372e3f75fa4af3ea7
Srikar-C <csrikar2003@gmail.com>
2022-06-08 10:37:57 +0530

    First Program

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+class HelloWorld{
+        public static void main(String args[]){
+                System.out.println("HEllO WORLD");
+                System.out.println("WELCOME THE WORLD")
+        }
+}
